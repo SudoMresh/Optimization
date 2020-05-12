00dd6a7e7f06fede5b03a7770143a6f49f6c1bcc
Fofanius <vova19007@gmail.com>
2020-05-12 12:05:32 +0300

    Логика бинарного дерева + представление
    - добавлена  логика бинарного дерева поиска в виде отдельной сборки (библиотеки)
    - добавлена страница (пока что пустая) для реализации UI для дерева

---
+++ Optimization/View/BinarySearchPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Optimization.View
+{
+    /// <summary>
+    /// Логика взаимодействия для BinarySearchPage.xaml
+    /// </summary>
+    public partial class BinarySearchPage : Page
+    {
+        public BinarySearchPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
