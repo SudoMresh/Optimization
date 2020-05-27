911bed5d734966f2bdd4d38b4cc2fc620a1da578
Iryna Kovalchuk <irina.alexandrovna.kovalchuk@gmail.com>
2020-05-28 00:35:44 +0300

    add Floyd Warshall algorithm

---
+++ Optimization/View/FloydWarshallAlgo.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows;
+using System.Security.Policy;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace Floyd_WarshallAlg
+{
+    class FloydWarshallAlgo
+    {
+
+        public const int INF = 10000;
+        int[,] distance;
+        int[,] parent;
+
+        public FloydWarshallAlgo()
+        {
+            this.distance = new int[6, 6]
+            {
+                { INF,   2,   1,   INF,   INF,   INF},
+                { INF, INF,   3,   3,   INF,   INF},
+                { INF, INF, INF,   INF,   1,   INF},
+                { INF, INF, INF, INF,   INF, 2},
+                { INF, INF, INF, 2,   INF,   5},
+                { INF, INF, INF, INF, INF, INF}
+            };
+        }
+
+        public void CreateMatrix(int size)
+        {
+            this.distance = new int[size, size];
+            this.parent = new int[size, size];
+
+            for (int i = 0; i < size; ++i)
+                for (int j = 0; j < size; ++j)
+                    this.distance[i, j] = INF;
+        }
+
+        public void AddVertex(int i, int j, int weight)
+        {
+            this.distance[i, j] = weight;
+        }
+
+        private static String MatrixToString(int[,] matrix) {
+            int verticesCount = matrix.GetLength(0);
+            StringBuilder sb = new StringBuilder();
+
+
+            for (int i = 0; i < verticesCount; ++i)
+            {
+                for (int j = 0; j < verticesCount; ++j)
+                {
+                    if (matrix[i, j] == INF)
+                        sb.Append("INF".PadLeft(6));
+                    else
+                        sb.Append(matrix[i, j].ToString().PadLeft(7));
+                }
+
+                sb.Append("\n");
+            }
+
+            return sb.ToString();
+        }
+
+        public String ToStringDistances()
+        {
+            return FloydWarshallAlgo.MatrixToString(this.distance);
+        }
+
+        public String ToStringParents()
+        {
+            return FloydWarshallAlgo.MatrixToString(this.parent);
+        }
+
+        public List<int> FindPath(int from, int to)
+        {
+            List<int> path = new List<int>();
+
+            path.Add(to);
+            int point = to;
+
+            while (point != from)
+            {
+                point = parent[from, point];
+                path.Add(point);
+            }
+
+            path.Reverse();
+
+            return path;
+        }
+
+        public void Solve()
+        {
+            int size = this.distance.GetLength(0);
+            this.parent = new int[size, size];
+
+            for (int i = 0; i < size; ++i)
+                for (int j = 0; j < size; ++j)
+                    this.parent[i, j] = i;
+
+            for (int k = 0; k < size; ++k)
+            {
+                for (int i = 0; i < size; ++i)
+                {
+                    for (int j = 0; j < size; ++j)
+                    {
+                        if (distance[i, k] + distance[k, j] < distance[i, j])
+                        {
+                            distance[i, j] = distance[i, k] + distance[k, j];
+                            parent[i, j] = parent[k, j];
+                        }
+                    }
+                }
+            }
+
+            for (int i = 0; i < size; ++i)
+            {
+                for (int j = 0; j < size; ++j)
+                {
+                    if (distance[i, j] == INF)
+                    {
+                        parent[i, j] = -1;
+                    }
+                    else
+                    {
+                        parent[i, j] = parent[i, j];
+                    }
+                }
+            }
+
+        }
+
+        public static void Run()
+        {
+            int[,] graph = {
+                         { INF,   2,   1,   INF,   INF,   INF},
+                         { INF, INF,   3,   3,   INF,   INF},
+                         { INF, INF, INF,   INF,   1,   INF},
+                         { INF, INF, INF, INF,   INF, 2},
+                         { INF, INF, INF, 2,   INF,   5},
+                         { INF, INF, INF, INF, INF, INF}
+                         };
+
+
+        }
+
+    }
+}
---
+++ Optimization/View/FloydWarshallPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Windows;
+using System.Windows.Controls;
+using FloydWarshall;
+
+namespace Optimization.View
+{
+    public partial class FloydWarshallPage : Page
+    {
+
+        FloydWarshallAlgo algorithm;
+
+        public FloydWarshallPage()
+        {
+            InitializeComponent();
+           
+            this.algorithm = new FloydWarshallAlgo();
+        }
+
+
+        private void HandleMatrixCreateButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (CountPoint.Text == "")
+            {
+                MessageBox.Show("Вы не ввели количество вершин!"); return;
+            }
+            int size = Int32.Parse(CountPoint.Text);
+
+            if (size <= 1) { MessageBox.Show("Количество вершин должно быть больше 1!"); return; }
+
+            this.algorithm.CreateMatrix(size);
+
+            this.RefreshResultTextWithMatrix();
+            
+        }
+
+        private void RefreshResultTextWithMatrix()
+        {
+            result.Text = "";
+
+            this.AppendResultText(this.algorithm.ToStringDistances());
+        }
+
+        private void AppendResultText(String txt)
+        {
+            result.Text = result.Text + "\n" + txt;
+        }
+
+        private void HandleAddVertexClick(object sender, RoutedEventArgs e)
+        {
+            if (!this.algorithm.IsCreated()) { MessageBox.Show("Матрица расстояний не создана!\nПопробуйте ввести количество вершин и нажать кнопку Создать матрицу расстояний."); return; }
+            
+            if (CountPoint.Text == "")
+            {
+                MessageBox.Show("Введите количество вершин!"); return;
+            }
+
+            int size = Int32.Parse(CountPoint.Text);
+
+            if (StartVertex.Text == "")
+            {
+                MessageBox.Show("Введите начальную вершину!"); return;
+            }
+            int startVertex = Int32.Parse(StartVertex.Text);
+
+            if (FinishVertex.Text == "")
+            {
+               MessageBox.Show("Введите конечную вершину!"); return;
+            }
+            int finishVertex = Int32.Parse(FinishVertex.Text);
+
+            if (startVertex == finishVertex) { MessageBox.Show("Вершины должны быть разные."); return; }
+
+            if (startVertex > size - 1 || finishVertex > size - 1) { MessageBox.Show("Предупреждение!\nОбратите внимание, что индексация для вершин начинает с 0 для записи их в матрицу расстояний и индекс не должен быть больше количества вершин!"); return; }
+
+            if (WeightEdge.Text == "")
+            {
+                MessageBox.Show("Введите вес для заданой вершины!"); return;
+            }
+            int edgeWeight = Int32.Parse(WeightEdge.Text);
+
+            this.algorithm.AddVertex(startVertex, finishVertex, edgeWeight);
+
+            this.RefreshResultTextWithMatrix();
+        }
+
+        private void HandleSolveClick(object sender, RoutedEventArgs e)
+        {
+            if (!this.algorithm.IsCreated()) { MessageBox.Show("Матрица расстояний не создана!\nПопробуйте ввести количество вершин и нажать кнопку Создать матрицу расстояний."); return; }
+
+            this.algorithm.Solve();
+
+            this.AppendResultText("Результат:\n" + this.algorithm.ToStringDistances());
+            this.AppendResultText("Матрица предков:\n" + this.algorithm.ToStringParents());
+        }
+
+        private void HandleFindPathClick(object sender, RoutedEventArgs e)
+        {
+            if (!this.algorithm.IsCreated()) { MessageBox.Show("Матрица расстояний не создана!\nПопробуйте ввести количество вершин и нажать кнопку Создать матрицу расстояний."); return; }
+
+            if (AddStartSolveVertex.Text == "")
+            {
+                MessageBox.Show("Введите начальную вершину для поиска пути!"); return;
+            }
+            int startVertexPath = Int32.Parse(AddStartSolveVertex.Text);
+
+            if (AddFinishSolveVertex.Text == "")
+            {
+                MessageBox.Show("Введите конечную вершину для поиска пути!"); return;
+            }
+            int finishVertexPath = Int32.Parse(AddFinishSolveVertex.Text);
+
+            if (AddStartSolveVertex.Text == "" && AddFinishSolveVertex.Text  == "")
+            {
+                MessageBox.Show("Введите начальную и конечную вершины для поиска пути!"); return;
+            }
+            int size = Int32.Parse(CountPoint.Text);
+
+            if (startVertexPath == finishVertexPath) { MessageBox.Show("Вершины должны быть разные."); return; }
+            if (startVertexPath > size - 1 || finishVertexPath > size - 1 || startVertexPath < 0 || finishVertexPath < 0) { MessageBox.Show("Предупреждение!\nОбратите внимание, что индексация для вершин начинает с 0 и индекс не должен быть больше количества вершин!"); return; }
+            
+            List<int> path = this.algorithm.FindPath(startVertexPath, finishVertexPath);
+            
+            this.AppendResultText("Путь:\n" + string.Join(" -> ", path.ToArray()));
+        }
+    }
+}
